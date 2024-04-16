f9581fa2d0acd2c5c92aadfd901b263983c0823f
U-BRCMLTD\su671781 <su671781@CTHNTV3.Broadcom.net>
2024-04-16 16:58:49 +0530

    Refactored code

--- modules/graphman-operation-validate.js
+++ modules/graphman-operation-validate.js
@@ -1,13 +1,13 @@
 const utils = require("./graphman-utils");
 const graphman = require("./graphman");
-
+var error = false;
 module.exports = {
     run: function (params) {
         if (params.input) {
             const obj = utils.readFile(params.input)
             if (Object.keys(obj).length == 1 && Object.keys(obj)[0] == "All") {
-                validateJson(Object.values(obj)[0])
-                if (!graphman.policySchema.errors) {
+                validateAssertions(Object.values(obj)[0])
+                if (!error) {
                     utils.info("validation is successful");
                 }
             } else {
@@ -21,14 +21,12 @@ module.exports = {
     }
 }
 
-function validateJson(allArray) {
-    for (let i = 0; i < allArray.length; i++) {
-        const key = Object.keys(allArray[i])[0]
+function validateAssertions(assertionArray) {
+    for (let i = 0; i < assertionArray.length; i++) {
+        validateAssertion(assertionArray[i])
+        const key = Object.keys(assertionArray[i])[0]
         if (key == "All" || key == "OneOrMore") {
-            validateAssertion(allArray[i])
-            validateJson(Object.values(allArray[i])[0])
-        } else {
-            validateAssertion(allArray[i])
+            validateAssertions(Object.values(assertionArray[i])[0])
         }
     }
 }
@@ -36,6 +34,7 @@ function validateJson(allArray) {
 function validateAssertion(assertion) {
     const valid = graphman.policySchema(assertion)
     if (!valid) {
+        error = true
         console.log(graphman.policySchema.errors)
     }
 }
