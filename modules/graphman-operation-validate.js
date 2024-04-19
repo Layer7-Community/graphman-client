ba0beb332c1401ccc8548f486e939441c08a448c
U-BRCMLTD\su671781 <su671781@CTHNTV3.Broadcom.net>
2024-04-19 18:02:49 +0530

    Address review comments

--- modules/graphman-operation-validate.js
+++ modules/graphman-operation-validate.js
@@ -2,19 +2,16 @@ const utils = require("./graphman-utils");
 const policySchema = require("./policy-schema");
 
 var error = false;
+var policyErrors = false;
 module.exports = {
     schemaValidator: null,
     run: function (params) {
         if (params.input) {
             buildPolicySchema()
             const obj = utils.readFile(params.input)
-            if (Object.keys(obj).length == 1 && Object.keys(obj)[0] == "All") {
-                validateAssertions(Object.values(obj)[0])
-                if (!error) {
-                    utils.info("validation is successful");
-                }
-            } else {
-                utils.info("Given policy is invalid");
+            validateBundle(obj);
+            if (!error) {
+                utils.info("validation is successful");
             }
         }
     },
@@ -23,7 +20,50 @@ module.exports = {
         utils.print("    validate --input <input-file>");
     }
 }
-function buildPolicySchema(){
+
+function validateBundle(obj) {
+    for (const [entityName, entities] of Object.entries(obj)) {
+        if (entityName == "services" || entityName == "policies") {
+            validateEntities(entities)
+        }
+    }
+
+}
+
+function validateEntities(entities) {
+    for (const entity of entities) {
+        policyErrors = false
+        var name
+        for (const [propertyName, propertyValue] of Object.entries(entity)) {
+            if (propertyName == "name") {
+                name = propertyValue
+            } else if (propertyName == "policy") {
+                validatePolicy(propertyValue)
+            }
+        }
+        if (policyErrors) {
+            utils.info("Invalid policy code in " + name);
+        }
+    }
+}
+
+function validatePolicy(policy) {
+    for (const [propertyName, propertyValue] of Object.entries(policy)) {
+        if (propertyName == "code") {
+            validatePolicyCode(propertyValue)
+        }
+    }
+}
+
+function validatePolicyCode(obj) {
+    if (Object.keys(obj).length == 1 && Object.keys(obj)[0] == "All") {
+        validateAssertions(Object.values(obj)[0])
+    } else {
+        utils.info("Given policy is invalid");
+    }
+}
+
+function buildPolicySchema() {
     this.schemaValidator = policySchema.build();
 }
 
@@ -41,6 +81,7 @@ function validateAssertion(assertion) {
     const valid = this.schemaValidator(assertion)
     if (!valid) {
         error = true
+        policyErrors = true
         utils.print(this.schemaValidator.errors)
     }
 }
