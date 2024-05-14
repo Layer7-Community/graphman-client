8d0a1eb84c910dc230696f344fbdd72689407e1b
graju256 <graju256@gmail.com>
2024-05-14 13:27:56 +0530

    NORALLY: extending the revise operation (v1.2.00) (#35)
    * NORALLY: extending the revise operation
    
    * fixing the renew operation
    
    * adding globalPolicies and backgroundTaskPolicies to the deprecated list
    
    * improving the unhandled error message
    
    * updated the deprecated entity types section of README
    
    * revise for ldap
    
    * updating the version
    
    * updating revise operation

--- modules/graphman-operation-revise.js
+++ modules/graphman-operation-revise.js
@@ -1,10 +1,11 @@
 
 const utils = require("./graphman-utils");
 const butils = require("./graphman-bundle");
+const reviser = require("./graphql-entity-reviser");
 
 module.exports = {
     /**
-     * Revises the input bundle for import operations.
+     * Revises the input bundle as per the options.
      * @param params
      * @param params.input input bundle
      * @param params.output output bundle
@@ -22,21 +23,21 @@ module.exports = {
             butils.removeDuplicates(bundle);
         }
 
-        this.revise(bundle);
+        this.revise(bundle, params.options);
         utils.writeResult(params.output, butils.sort(bundle));
     },
 
-    revise: function (bundle) {
-        const goidMappings = bundle.goidMappings || [];
-        const guidMappings = bundle.guidMappings || [];
-        delete bundle.goidMappings;
-        delete bundle.guidMappings;
+    revise: function (bundle, options) {
+        bundle = reviseBundleForGoidsAndGuids(bundle);
 
-        Object.keys(bundle).filter(key => Array.isArray(bundle[key])).forEach(key => {
-            utils.info("inspecting " + key);
-            if (goidMappings.length) reviseEntities(bundle[key], goidMappings);
-            if (guidMappings.length) reviseEntities(bundle[key], guidMappings);
-        });
+        // remove properties section from the bundle temporarily
+        const properties = bundle.properties;
+        if (properties) delete bundle.properties;
+
+        bundle = reviser.revise(bundle, options);
+
+        // restore the properties section so that it appears at the end
+        if (properties) bundle.properties = properties;
 
         return bundle;
     },
@@ -55,7 +56,9 @@ module.exports = {
         console.log("  [--output <output-file>]");
         console.log("  [--options.<name> <value>,...]");
         console.log();
-        console.log("Revises the input bundle (which is likely to be result of diff command) as per the GOID and/or GUID mappings");
+        console.log("Revises the input bundle as per the specified options.");
+        console.log("  supports revising the bundle as per the GOID and/or GUID mappings");
+        console.log("  supports revising the bundle as per the schema changes");
         console.log();
         console.log("  --input <input-file>");
         console.log("    specify the name of input bundle file that contains gateway configuration");
@@ -71,6 +74,23 @@ module.exports = {
         console.log("      .excludeGoids");
         console.log("        use this option to exclude Goids from the bundled entities. This option is applicable only when normalize option is selected.");
     }
+}
+
+function reviseBundleForGoidsAndGuids (bundle) {
+    const goidMappings = bundle.goidMappings || [];
+    const guidMappings = bundle.guidMappings || [];
+    delete bundle.goidMappings;
+    delete bundle.guidMappings;
+
+    if (goidMappings.length > 0 || guidMappings.length > 0) {
+        Object.keys(bundle).filter(key => Array.isArray(bundle[key])).forEach(key => {
+            utils.info("inspecting " + key);
+            if (goidMappings.length) reviseEntities(bundle[key], goidMappings);
+            if (guidMappings.length) reviseEntities(bundle[key], guidMappings);
+        });
+    }
+
+    return bundle;
 }
 
 function reviseEntities(entities, mappings) {
@@ -90,3 +110,4 @@ function reviseEntity(entity, mappings) {
         });
     });
 }
+
