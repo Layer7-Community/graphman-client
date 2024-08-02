d23c75d282c5cb20b62ada016ee32611a5eef9bc
Raju Gurram <raju.gurram@broadcom.com>
2024-08-02 12:59:32 +0530

    updating the build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 
 distDir=build/dist
-packageDir=build/layer7-graphman-client
-wrapperDir=build/layer7-graphman-client-wrapper
+packageDir=build/layer7-graphman
+wrapperDir=build/layer7-graphman-wrapper
 
 # copy required files to build main package
 echo cleaning build directory
@@ -16,6 +16,7 @@ echo building [@layer7/graphman] package
 cp -r modules $packageDir/
 cp -r queries $packageDir/
 cp -r schema $packageDir/
+cp LICENSE.md $packageDir/
 cp package.json $packageDir/
 pushd $packageDir>/dev/null
 rm -rf modules/graphman-extension-*.js
@@ -28,10 +29,13 @@ mkdir $wrapperDir/queries
 cp modules/graphman-extension-*.js $wrapperDir/modules/.
 cp cli-main.js $wrapperDir/.
 cp graphman.* $wrapperDir/.
-tar -czvf build/layer7-graphman-wrapper.tar.gz $wrapperDir
+cp LICENSE.md $wrapperDir/
+pushd build>/dev/null
+tar -czvf layer7-graphman-wrapper.tar.gz *-wrapper
+popd>/dev/null
 
-mv $packageDir/layer7-graphman-*.tgz build/dist/.
-mv build/layer7-graphman-wrapper.tar.gz build/dist/.
+mv $packageDir/*.tgz build/dist/.
+mv build/*.tar.gz build/dist/.
 echo build completed
 echo distribution packages
 ls -l build/dist
