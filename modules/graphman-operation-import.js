b34190bd378f5c5e65cfd1a549e6059a4553001b
Raju Gurram <raju.gurram@broadcom.com>
2024-09-04 08:43:22 +0530

    checking the query validity during the import operation

--- modules/graphman-operation-import.js
+++ modules/graphman-operation-import.js
@@ -41,6 +41,11 @@ module.exports = {
         inputBundle = utils.extension("pre-import").apply(inputBundle, params.options);
 
         const query = gql.generate(params.using, Object.assign(inputBundle, params.variables), params.options);
+        if (!query.query.startsWith("mutation")) {
+            utils.info("invalid query for import operation", query);
+            throw "invalid query for import operation";
+        }
+
         const request = graphman.request(gateway, query.options);
         delete query.options;
         request.body = query;
