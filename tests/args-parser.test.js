329693a01905f3fa57f466b8ae627581f01b9559
Raju Gurram <raju.gurram@broadcom.com>
2026-09-07 09:46:28 +0530

    support for array constructions via CLI args

--- tests/args-parser.test.js
+++ tests/args-parser.test.js
@@ -44,3 +44,47 @@ test("test complex arguments", () => {
         "includeRoles": true
     });
 });
+
+test("test controlled array of plain values using trailing +", () => {
+    const params = parser.parse([
+        "--roles.+", "Admin",
+        "--roles.+", "User"
+    ]);
+
+    expect(params).toMatchObject({
+        "roles": ["Admin", "User"]
+    });
+});
+
+test("test controlled array of records using mid-path +", () => {
+    const params = parser.parse([
+        "--input.+.name", "foo",
+        "--input.value", "bar",
+        "--input.+.name", "baz",
+        "--input.value", "qux"
+    ]);
+
+    expect(params).toMatchObject({
+        "input": [
+            {"name": "foo", "value": "bar"},
+            {"name": "baz", "value": "qux"}
+        ]
+    });
+});
+
+test("test controlled array of records with a nested leaf array", () => {
+    const params = parser.parse([
+        "--rules.+.name", "r1",
+        "--rules.roles.+", "Admin",
+        "--rules.roles.+", "Editor",
+        "--rules.+.name", "r2",
+        "--rules.roles.+", "Viewer"
+    ]);
+
+    expect(params).toMatchObject({
+        "rules": [
+            {"name": "r1", "roles": ["Admin", "Editor"]},
+            {"name": "r2", "roles": ["Viewer"]}
+        ]
+    });
+});
